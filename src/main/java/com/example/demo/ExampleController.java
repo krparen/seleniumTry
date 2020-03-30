1234df0c295bfeacd6b21ce093c00c9a80b3b2b9
g.eremeev <g.eremeev@azoft.com>
2020-03-30 15:40:02 +0300

    city to choice and button click working

--- src/main/java/com/example/demo/ExampleController.java
+++ src/main/java/com/example/demo/ExampleController.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,17 +18,36 @@ import javax.annotation.PostConstruct;
 @RequestMapping("/getSiteContent")
 public class ExampleController {
 
+    private String FROM_BLOCK_XPATH = "//div[@class = 'group-fields__from-block']/location-autocomplete/div";
+    private String TO_BLOCK_XPATH = "";
+
     @RequestMapping
     public String getSiteContent() throws Exception {
         WebDriver driver = new ChromeDriver();
         driver.get("https://spasibosberbank.travel/#/");
-        Thread.sleep(5000);
+        WebDriverWait wait = new WebDriverWait(driver,30);
+        //wait.until(ExpectedConditions.visibilityOfElementLocated())
+        Thread.sleep(2000);
         WebElement inputFrom = driver.findElement(By.xpath("//div[@class = 'group-fields__from-block']/location-autocomplete/div/input"));
         inputFrom.clear();
         inputFrom.sendKeys("Москва");
-        Thread.sleep(5000);
+        Thread.sleep(2000);
         WebElement searchResult = driver.findElement(By.xpath("//div[@class = 'group-fields__from-block']/location-autocomplete/div/ul/li"));
         searchResult.click();
+
+        // autocomplete__close
+
+        WebElement inputTo = driver.findElement(By.xpath("//div[@class = 'search-form__group-fields']/location-autocomplete/div/input"));
+        inputTo.clear();
+        inputTo.sendKeys("Новосибирск");
+        Thread.sleep(2000);
+        WebElement searchResultTo = driver.findElement(By.xpath("//div[@class = 'search-form__group-fields']/location-autocomplete/div/ul/li"));
+        searchResultTo.click();
+
+        WebElement submitButton = driver.findElement(By.className("search-form__submit"));
+        submitButton.click();
+
+        //search-form__submit
         return inputFrom.toString();
     }
 
