f5bd25db84a6722b3664c3fc81f5700809a9d056
g.eremeev <g.eremeev@azoft.com>
2020-03-30 13:43:40 +0300

    city from choice working

--- src/main/java/com/example/demo/ExampleController.java
+++ src/main/java/com/example/demo/ExampleController.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,20 +12,30 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.PostConstruct;
 
 @RestController
+@Slf4j
 @RequestMapping("/getSiteContent")
 public class ExampleController {
 
     @RequestMapping
-    public String getSiteContent() {
+    public String getSiteContent() throws Exception {
         WebDriver driver = new ChromeDriver();
-        WebElement inputFrom = driver.findElement(By.xpath("//div[@class = 'group-fields__from-block']/location-autocomplete/div"));
         driver.get("https://spasibosberbank.travel/#/");
-//        return driver.getPageSource();
+        Thread.sleep(5000);
+        WebElement inputFrom = driver.findElement(By.xpath("//div[@class = 'group-fields__from-block']/location-autocomplete/div/input"));
+        inputFrom.clear();
+        inputFrom.sendKeys("Москва");
+        Thread.sleep(5000);
+        WebElement searchResult = driver.findElement(By.xpath("//div[@class = 'group-fields__from-block']/location-autocomplete/div/ul/li"));
+        searchResult.click();
         return inputFrom.toString();
+    }
+
+    private WebElement cityFromAutocomplete() {
+
     }
 
     @PostConstruct
     public void postConstruct() {
-        System.setProperty("webdriver.chrome.driver","C:/chromedriver/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:/chromedriver/chromedriver.exe");
     }
 }
