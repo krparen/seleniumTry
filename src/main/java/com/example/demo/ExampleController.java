b10f66b0698a4a7f4d8d59db52c97ea503f5a634
g.eremeev <g.eremeev@azoft.com>
2020-03-30 12:03:52 +0300

    alpha version

--- src/main/java/com/example/demo/ExampleController.java
+++ src/main/java/com/example/demo/ExampleController.java
@@ -1,7 +1,13 @@
 package com.example.demo;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
 
 @RestController
 @RequestMapping("/getSiteContent")
@@ -9,6 +15,15 @@ public class ExampleController {
 
     @RequestMapping
     public String getSiteContent() {
-        return "Типа сайт контент";
+        WebDriver driver = new ChromeDriver();
+        WebElement inputFrom = driver.findElement(By.xpath("//div[@class = 'group-fields__from-block']/location-autocomplete/div"));
+        driver.get("https://spasibosberbank.travel/#/");
+//        return driver.getPageSource();
+        return inputFrom.toString();
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        System.setProperty("webdriver.chrome.driver","C:/chromedriver/chromedriver.exe");
     }
 }
